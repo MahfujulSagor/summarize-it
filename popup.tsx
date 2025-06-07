dc9e241edf4ad911d3e22ba20a156c49b3952459
MahfujulSagor <me24020@mbstu.ac.bd>
2025-06-07 19:09:27 +0600

    first commit

--- popup.tsx
+++ popup.tsx
@@ -10,13 +10,13 @@ function IndexPopup() {
       }}>
       <h2>
         Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
+        <a href="#" target="_blank">
           Plasmo
         </a>{" "}
         Extension!
       </h2>
       <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
+      <a href="#" target="_blank">
         View Docs
       </a>
     </div>
